eda9010a3e38ed45e2668e6ef0a4a471e9b40500
bakeraw2 <michalskidaniel2@gmail.com>
2014-05-23 22:14:37 +0200

    poprawki

---
+++ R13_Using_MessageSource/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import shapes.Shape;
+
+/**
+ * Author: Daniel
+ */
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("context.xml");
+
+        Shape circle = (Shape) context.getBean("circle");
+        circle.draw();
+    }
+}
